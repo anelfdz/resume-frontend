d76324ddbf69aa76480c2513cda5286fd3dd703a
Anel Fernandez <anel.fernandez@mercadolibre.cl>
2025-05-23 10:06:10 -0400

    Uninstall fingerprintjs

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,26 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {useVisitorData} from '@fingerprintjs/fingerprintjs-pro-react'
 import styles from './Profile.module.css';
 
 const Profile = ({profile}) => {
-    const {isLoading, error, data} = useVisitorData(
-        {extendedResult: true},
-        {immediate: true}
-    );
-
-    useEffect(() => {
-        if (isLoading) {
-            console.log('Fingerprint is loading...');
-        }
-        if (data) {
-            console.log('Fingerprint Data:', data);
-        }
-        if (error) {
-            console.error('Fingerprint Error:', error);
-        }
-    }, [isLoading, data, error]);
-
     return (
         <section className={styles.profile}>
             <h2 className={styles.profileTitle}>Profile</h2>
--- src/index.js
+++ src/index.js
@@ -3,28 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
-import {
-  FpjsProvider,
-  FingerprintJSPro
-} from '@fingerprintjs/fingerprintjs-pro-react'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FpjsProvider
-      loadOptions={{
-        apiKey: "lnwCCf88WMUrFyf4MA3i",
-        endpoint: [
-          "https://anelfdz.com/x2ng7V5plTxX0JjY/7MoMUqe42O8t7ACh",
-          FingerprintJSPro.defaultEndpoint
-        ],
-        scriptUrlPattern: [
-          "https://anelfdz.com/x2ng7V5plTxX0JjY/3eFIYOqVDftPPd3i?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>",
-          FingerprintJSPro.defaultScriptUrlPattern
-        ]
-      }}
-    >
       <App />
-    </FpjsProvider>
   </React.StrictMode>
 );
